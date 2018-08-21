2e697112b86e092e83d8a28e505f49e0995bce03
Matthieu Bovel <matthieu@bovel.net>
2018-08-21 16:07:01 +0200

    Allow external access of selection

--- src/Model.ts
+++ src/Model.ts
@@ -3,27 +3,31 @@ import { Tree } from "ts-tree";
 import { ModelEvent } from "./ModelEvent";
 
 export class Model<V> {
-	private clipboard: Tree<V>[] = [];
-	private selection: Set<Tree<V>> = new Set();
-	private cursor?: Tree<V>;
-	private pubsub: PubSub<ModelEvent<V>> = new PubSub();
+	private _clipboard: Tree<V>[] = [];
+	private _selection: Set<Tree<V>> = new Set();
+	private _cursor?: Tree<V>;
+	private _pubsub: PubSub<ModelEvent<V>> = new PubSub();
 
 	constructor(readonly root: Tree<V>, readonly isLeaf: (tree: Tree<V>) => boolean) {}
 
+	get selection(): ReadonlySet<Tree<V>> {
+		return this._selection;
+	}
+
 	subscribe(fn: (e: ModelEvent<V>) => any) {
-		this.pubsub.subscribe(fn);
+		this._pubsub.subscribe(fn);
 		this.emitTree(this.root);
 	}
 
 	unsubscribe(fn: (e: ModelEvent<V>) => any) {
-		this.pubsub.unsubscribe(fn);
+		this._pubsub.unsubscribe(fn);
 	}
 
 	selectOne(tree?: Tree<V>) {
-		if (tree && this.selection.size === 1 && this.selection.has(tree)) {
+		if (tree && this._selection.size === 1 && this._selection.has(tree)) {
 			return;
 		}
-		for (const node of this.selection) {
+		for (const node of this._selection) {
 			this.removeFromSelection(node);
 		}
 		if (!tree) {
@@ -35,15 +39,15 @@ export class Model<V> {
 	}
 
 	selectPrev() {
-		this.selectOne(this.cursor && this.cursor.previous);
+		this.selectOne(this._cursor && this._cursor.previous);
 	}
 
 	selectNext() {
-		this.selectOne(this.cursor && this.cursor.next);
+		this.selectOne(this._cursor && this._cursor.next);
 	}
 
 	selectToggle(tree: Tree<V>) {
-		if (this.selection.has(tree)) {
+		if (this._selection.has(tree)) {
 			this.unselect(tree);
 		} else {
 			this.addToSelection(tree);
@@ -52,12 +56,12 @@ export class Model<V> {
 	}
 
 	selectUntil(tree: Tree<V>) {
-		if (!this.cursor) {
+		if (!this._cursor) {
 			return;
 		}
-		const isBefore = tree.isBefore(this.cursor) < 0;
-		let current: Tree<V> | undefined = isBefore ? tree : this.cursor.next;
-		const end: Tree<V> | undefined = isBefore ? this.cursor : tree.next;
+		const isBefore = tree.isBefore(this._cursor) < 0;
+		let current: Tree<V> | undefined = isBefore ? tree : this._cursor.next;
+		const end: Tree<V> | undefined = isBefore ? this._cursor : tree.next;
 		while (current && current !== end) {
 			this.addToSelection(current);
 			current = current.next;
@@ -72,7 +76,7 @@ export class Model<V> {
 	}
 
 	resetSelection() {
-		for (const node of this.selection) {
+		for (const node of this._selection) {
 			this.removeFromSelection(node);
 		}
 		this.ensureValidCursor();
@@ -84,21 +88,21 @@ export class Model<V> {
 	}
 
 	copy(): void {
-		this.clipboard = this.selectedSubtrees.map(t => t.clone());
-		this.clipboard.reverse();
+		this._clipboard = this.selectedSubtrees.map(t => t.clone());
+		this._clipboard.reverse();
 	}
 
 	paste(): void {
-		if (!this.cursor) {
+		if (!this._cursor) {
 			return;
 		}
-		const isLeaf = this.isLeaf(this.cursor);
-		const parent = isLeaf ? this.cursor.parent : this.cursor;
-		const previousSibling = isLeaf ? this.cursor : undefined;
+		const isLeaf = this.isLeaf(this._cursor);
+		const parent = isLeaf ? this._cursor.parent : this._cursor;
+		const previousSibling = isLeaf ? this._cursor : undefined;
 		if (!parent) {
 			return;
 		}
-		for (const tree of this.clipboard) {
+		for (const tree of this._clipboard) {
 			this.insertAfter(parent, previousSibling, tree.clone());
 		}
 	}
@@ -112,38 +116,38 @@ export class Model<V> {
 
 	private insertAfter(parent: Tree<V>, reference: Tree<V> | undefined, tree: Tree<V>) {
 		parent.insertAfter(reference, tree);
-		this.pubsub.emit({ type: "insert", tree });
+		this._pubsub.emit({ type: "insert", tree });
 		this.emitTree(tree.firstChild);
 	}
 
 	private remove(tree: Tree<V>) {
 		this.removeFromSelection(tree);
 		tree.remove();
-		this.pubsub.emit({ type: "remove", tree });
+		this._pubsub.emit({ type: "remove", tree });
 	}
 
 	private addToSelection(tree: Tree<V>) {
-		if (!this.selection.has(tree)) {
-			this.selection.add(tree);
-			this.pubsub.emit({ type: "add-to-selection", tree });
+		if (!this._selection.has(tree)) {
+			this._selection.add(tree);
+			this._pubsub.emit({ type: "add-to-selection", tree });
 		}
 	}
 
 	private removeFromSelection(tree: Tree<V>) {
-		if (this.selection.delete(tree)) {
-			this.pubsub.emit({ type: "remove-from-selection", tree });
+		if (this._selection.delete(tree)) {
+			this._pubsub.emit({ type: "remove-from-selection", tree });
 		}
 	}
 
 	private setCursor(tree?: Tree<V>) {
-		if (tree !== this.cursor) {
-			this.pubsub.emit({ type: "move-cursor", tree });
+		if (tree !== this._cursor) {
+			this._pubsub.emit({ type: "move-cursor", tree });
 		}
-		this.cursor = tree;
+		this._cursor = tree;
 	}
 
 	private get sortedSelection() {
-		const result = [...this.selection];
+		const result = [...this._selection];
 		result.sort((a, b) => a.isBefore(b));
 		return result;
 	}
@@ -161,8 +165,8 @@ export class Model<V> {
 	}
 
 	private ensureValidCursor() {
-		if (this.cursor && this.cursor.root !== this.root) {
-			const first = this.selection.values().next();
+		if (this._cursor && this._cursor.root !== this.root) {
+			const first = this._selection.values().next();
 			this.setCursor(first.done ? undefined : first.value);
 		}
 	}
@@ -172,7 +176,7 @@ export class Model<V> {
 			return;
 		}
 		this.emitTree(tree.nextSibling);
-		this.pubsub.emit({ type: "insert", tree });
+		this._pubsub.emit({ type: "insert", tree });
 		this.emitTree(tree.firstChild);
 	}
 }
